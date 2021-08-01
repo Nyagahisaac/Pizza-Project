64e9c4f30590361714f059ae1e8f26481c1048a1
isaac.ireri <isaac.ireri@student.moringaschool.com>
2021-08-01 22:21:46 +0300

    add javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+function getSizePrice(sizePizza){
+    var pizaPrice = {
+      "extraLarge": 1200 ,
+       "large": 1000,
+       "medium": 800,
+       "small": 600
+    }
+    return pizaPrice[sizePizza];
+}
